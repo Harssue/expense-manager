f157aa1b6a4de57b40c92669bd87b54128ae5814
Harssue <hkothari2006@gmail.com>
2026-01-09 23:53:49 +0530

    Deployment

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-!pzx2ua!tx(comf6aer$h0ddqe*#)og=@#(*xhgb3+a-rep_7%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -149,3 +149,6 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5174",
     "http://127.0.0.1:5174",
 ]
+
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
